ec58239662a7d9acef72ac3ec5f4e1c735c4b27b
arikash117 <ari.kash117@gmail.com>
2025-04-01 09:53:09 +0300

    fixed test issues

--- backend/tests/api_tests.py
+++ backend/tests/api_tests.py
@@ -14,5 +14,11 @@ def test_get_by_unknown_user():
 def test_get_by_known_user():
     response = client.get("/timoha2007/clients/", params={"password":"mashedpotatoes"})
     assert response.status_code == 200
-    # assert response.json()[2]["birthday"] == "2001.01.01"
-    assert response.json()[3]["id"] == 13
+    assert response.json()[2]["birthday"] == "2001.01.01"
+    print(response.json())
+
+def test_get_all():
+    response = client.get("/users/")
+    assert response.status_code == 200
+    assert response.json()[0]['full_name'] == "Иванов Иван Иванович"
+    assert len(response.json()) == 6
