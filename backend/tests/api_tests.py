9723cb83db480f8530bd223c56a750a31ca63c46
arikash117 <ari.kash117@gmail.com>
2025-04-08 14:08:43 +0300

    improved code readability

--- backend/tests/api_tests.py
+++ backend/tests/api_tests.py
@@ -6,18 +6,23 @@ from main import app
 
 client = TestClient(app)
 
-def test_get_by_unknown_user():
-    response = client.get("/sun123/clients/", params={"password":"purupuru27"})
+def get_unknown_user():
+    response = client.get(
+        "/sun123/clients/",
+        params={"password":"purupuru27"}
+        )
     assert response.status_code == 200
     assert len(response.json()) == 0 
 
-def test_get_by_known_user():
-    response = client.get("/timoha2007/clients/", params={"password":"mashedpotatoes"})
+def get_known_user():
+    response = client.get(
+        "/timoha2007/clients/",
+        params={"password":"mashedpotatoes"}
+        )
     assert response.status_code == 200
     assert response.json()[2]["birthday"] == "2001.01.01"
-    print(response.json())
 
-def test_get_all():
+def get_all():
     response = client.get("/users/")
     assert response.status_code == 200
     assert response.json()[0]['full_name'] == "Иванов Иван Иванович"
